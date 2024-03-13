80b64be55346c227b170aad7b241c547ce26e29c
VoxLight <VoxLight@users.noreply.github.com>
2024-03-13 17:46:24 +0000

    Prod lanches normally, Dev lanches for pytest

--- run.sh
+++ run.sh
@@ -1,3 +1,14 @@
-docker-compose down
-docker-compose build
-docker-compose up
+if [ "$1" == "dev" ]; then
+    docker-compose -f docker-compose.dev.yml down
+    docker-compose -f docker-compose.dev.yml build
+    docker-compose -f docker-compose.dev.yml up
+    exit 0
+elif [ "$1" == "prod" ]; then
+    docker-compose -f docker-compose.prod.yml down
+    docker-compose -f docker-compose.prod.yml build
+    docker-compose -f docker-compose.prod.yml up
+    exit 0
+else
+    echo "Invalid argument. Please use 'dev' or 'prod'"
+    exit 1
+fi
